3a70afd5cf81d6b4cb1fa731977af2e13379387a
dva2vas <dva_vas@i.ua>
2024-08-22 16:10:24 +0300

    add posts view

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('hello/<int:user_id>/', views.hello, name='hello')
+    path('hello/<int:user_id>/', views.hello, name='hello'),
+    path('posts/', views.posts, name='posts'),
+    path('posts/<slug>', views.postdetail, name='postdetail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from blog import models
+
 
 def hello(request: HttpRequest, user_id: int):
     context = {'user_id': user_id}
     return render(request, 'blog/hello.html', context)
+
+
+def posts(request: HttpRequest):
+    posts = models.Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'blog/posts.html', context)
